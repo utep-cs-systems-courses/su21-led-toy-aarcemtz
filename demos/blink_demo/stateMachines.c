15753d5d2cd93a67adacd00245601eb6b87f9abf
aarcemtz <alfredo.arcemtz@gmail.com>
2021-07-11 21:24:08 -0600

    dim works

--- demos/blink_demo/stateMachines.c
+++ demos/blink_demo/stateMachines.c
@@ -1,11 +1,11 @@
 #include <msp430.h>
 #include "stateMachines.h"
 #include "led.h"
+#include "switches.h"
 
-char toggle_red()		/* always toggle! */
+char toggle_red()/* always toggle! */
 {
   static char state = 0;
-
   switch (state) {
   case 0:
     red_on = 1;
@@ -16,10 +16,9 @@ char toggle_red()		/* always toggle! */
     state = 0;
     break;
   }
-  return 1;			/* always changes an led */
+  return 1;/* always changes an led */
 }
-
-char toggle_green()	/* only toggle green if red is on!  */
+char toggle_green()/* only toggle green if red is on!  */
 {
   char changed = 0;
   if (red_on) {
@@ -29,20 +28,31 @@ char toggle_green()	/* only toggle green if red is on!  */
   return changed;
 }
 
+void blink_button_advance(){
 
-void state_advance()		/* alternate between toggling red & green */
-{
-  char changed = 0;  
-
-  static enum {R=0, G=1} color = G;
-  switch (color) {
-  case R: changed = toggle_red(); color = G; break;
-  case G: changed = toggle_green(); color = R; break;
+  static char state = 0;
+  switch (state) {
+  case 0:
+    red_on = 1;
+    state = 1;
+    break;
+  case 1:
+    red_on = 0;
+    state = 0;
+    break;
   }
+  return 1;
+}
 
-  led_changed = changed;
+void state_advance(){
+  red_on=1;
+  led_changed = 1;
   led_update();
 }
 
+void state_advance_1(){
 
-
+  red_on=0;
+  led_changed=1;
+  led_update();
+}
--- demos/blink_demo/wdInterruptHandler.c
+++ demos/blink_demo/wdInterruptHandler.c
@@ -1,11 +1,19 @@
 #include <msp430.h>
 #include "stateMachines.h"
+#include "led.h"
 
 void
-__interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
+__interrupt_vec(WDT_VECTOR) WDT(){
   static char blink_count = 0;
-  if (++blink_count == 125) {
-    state_advance();
-    blink_count = 0;
+  char interruptTime = 250;                   // This is one second
+
+  if(blink_count == interruptTime) {          // Change to zero to make brightest
+    state_advance();                          // Turn on red
+    blink_count=0;
+    //state_advance_1();                      // Uncomment to make most dim
+  }
+  else if(blink_count <= interruptTime){
+    state_advance_1();                        // Turn off red
+    blink_count++;
   }
 }
