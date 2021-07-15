237de4ed83a512a24ab993bead873ef8fc27997a
aarcemtz <alfredo.arcemtz@gmail.com>
2021-07-14 18:51:08 -0600

    catching up. count 3

---
+++ project/myProject/stateMachines.h
@@ -0,0 +1,10 @@
+#ifndef stateMachine_included
+#define stateMachine_included
+
+void state_advance();
+void state_advance_1();
+void turn_off_red();
+void turn_on_red();
+void blink_button_advance();
+
+#endif // included
